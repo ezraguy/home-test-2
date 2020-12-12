9d664f3097a4fa4848a2acb793e27ed36a157474
ezraguy <55058653+ezraguy@users.noreply.github.com>
2020-12-12 20:49:02 +0200

    returning the es6

--- js/index.js
+++ js/index.js
@@ -1,28 +1,27 @@
-var box = document.getElementById("box-svg");
-var lid = document.getElementById("box-lid");
-var bows = document.getElementById("box-bows");
-var confetti = document.getElementById("confetti");
-var productBox = document.getElementById("product-wrap");
-var light = document.getElementById("light-wrap");
-var desc = document.getElementById("product-desc");
-
-var init = function init() {
+const box = document.getElementById('box-svg');
+const lid = document.getElementById('box-lid');
+const bows = document.getElementById('box-bows');
+const confetti = document.getElementById('confetti');
+const productBox = document.getElementById('product-wrap');
+const light = document.getElementById('light-wrap');
+const desc = document.getElementById('product-desc');
+const init = () => {
     addAnimations();
-};
+}
+const addConfetti = () => {
+    productBox.style.animation = ' rise 1s ease forwards 300ms'
+    light.style.animation = ' shine 1.5s ease forwards 100ms'
+    desc.style.animation = ' showText 1s ease forwards 700ms'
+    confetti.click()
 
-var addConfetti = function addConfetti() {
-    productBox.style.animation = " rise 1s ease forwards 300ms";
-    light.style.animation = " shine 1.5s ease forwards 100ms";
-    desc.style.animation = " showText 1s ease forwards 700ms";
-    confetti.click();
-};
+}
 
-var addAnimations = function addAnimations() {
-    setTimeout(function () {
-        bows.style.animation = "pop  1s  ease forwards";
-        lid.style.animation = "pop  1s  ease forwards";
+const addAnimations = () => {
+    setTimeout(() => {
+        bows.style.animation = 'pop  1s  ease forwards'
+        lid.style.animation = 'pop  1s  ease forwards'
         addConfetti();
     }, 4000);
-};
+}
 
 init();
